dea9eb4949c8917b013cba431de337801b913f3d
krismos64 <c.mostefaoui@yahoo.fr>
2025-02-02 14:31:16 +0100

    update port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,7 @@ import path from "path";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.SERVER_PORT || 5001;
+const PORT = process.env.SERVER_PORT || 5002;
 
 // Middlewares
 app.use(cors());
